3307f876ef979c08e46458f827019c0e0570ab25
me <me@myself.com>
2018-06-15 19:36:42 -0400

    Add tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,12 +3,21 @@ import { AppPage } from './app.po';
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeAll(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ng6!');
+  it('should display Menu', () => {
+    expect(page.getParagraphText()).toEqual('Menu');
+  });
+
+  it('should have  3 menu items', () => {
+    page.getMenuList().then((items) => {
+      expect(items.length).toBe(3);
+      expect(items[0].getText()).toBe('First Page');
+      expect(items[1].getText()).toBe('Editable Data Table');
+      expect(items[2].getText()).toBe('Reactive Forms');
+    });
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,15 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root my-nav mat-sidenav-container mat-sidenav mat-toolbar')).getText();
+  }
+
+  getMenuList() {
+    return new Promise((resolve, reject) => {
+      element(by.css('.mat-nav-list')).all(by.css('.mat-list-item')).then(function (items) {
+        resolve(items);
+      },
+        (err) => { reject(err)});
+    });
   }
 }
